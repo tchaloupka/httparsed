6ba3c4362672c8547a4aebcd476f30664196a6bb
Tomáš Chaloupka <chalucha@gmail.com>
2020-12-15 19:14:38 +0100

    codecov

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,18 +10,25 @@ if [ $DC = "ldc2" ]; then DC="ldmd2"; fi
 
 rm -f *-unittest*
 
-# unittests no SSE
-$DC -version=UTMAIN -debug -g -unittest -w -vcolumns -of=httparsed-unittest $SRC_FILES
-./httparsed-unittest
+if [ "$COVERAGE" = true ]; then
+    $DC -version=UTMAIN -cov -debug -g -unittest -w -vcolumns -of=httparsed-unittest-cov $SRC_FILES
+    ./httparsed-unittest-cov
+    wget https://codecov.io/bash -O codecov.sh
+    bash codecov.sh
+else
+    # unittests no SSE
+    $DC -version=UTMAIN -debug -g -unittest -w -vcolumns -of=httparsed-unittest $SRC_FILES
+    ./httparsed-unittest
 
-# unittests with possible SSE
-$DC -version=UTMAIN -debug -g -unittest -w -vcolumns -mcpu=native -of=httparsed-sse-unittest $SRC_FILES
-./httparsed-sse-unittest
+    # unittests with possible SSE
+    $DC -version=UTMAIN -debug -g -unittest -w -vcolumns -mcpu=native -of=httparsed-sse-unittest $SRC_FILES
+    ./httparsed-sse-unittest
 
-# betterC unitests no SSE
-$DC -version=UTMAIN -debug -g -unittest -w -vcolumns -betterC -of=httparsed-bc-unittest $SRC_FILES
-./httparsed-bc-unittest
+    # betterC unitests no SSE
+    $DC -version=UTMAIN -debug -g -unittest -w -vcolumns -betterC -of=httparsed-bc-unittest $SRC_FILES
+    ./httparsed-bc-unittest
 
-# betterC unitests with possible SSE
-$DC -version=UTMAIN -debug -g -unittest -w -vcolumns -mcpu=native -betterC -of=httparsed-bc-sse-unittest $SRC_FILES
-./httparsed-bc-sse-unittest
+    # betterC unitests with possible SSE
+    $DC -version=UTMAIN -debug -g -unittest -w -vcolumns -mcpu=native -betterC -of=httparsed-bc-sse-unittest $SRC_FILES
+    ./httparsed-bc-sse-unittest
+fi
