1bc45b05a27083d67ab8c4c9e11a43500f1fcba7
Tomáš Chaloupka <chalucha@gmail.com>
2025-03-08 10:03:11 +0100

    update CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,35 +4,35 @@ SRC_FILES="-Isource/ source/*.d"
 
 set -v -e -o pipefail
 
-if [ -z $DC ]; then DC="dmd"; fi
+if [ -z $DC ]; then DMD="dmd"; fi
 
-if [ $DC = "ldc2" ]; then DC="ldmd2"; fi
+if [ $DC = "ldc2" ]; then DMD="ldmd2"; fi
 
 rm -f *test*
 
 if [ "$COVERAGE" = true ]; then
-    $DC -version=CI_MAIN -cov -debug -g -unittest -w -vcolumns -of=httparsed-unittest-cov $SRC_FILES
+    $DMD -version=CI_MAIN -cov -debug -g -unittest -w -vcolumns -of=httparsed-unittest-cov $SRC_FILES
     ./httparsed-unittest-cov
     wget https://codecov.io/bash -O codecov.sh
     bash codecov.sh
 else
     # unittests no SSE
-    $DC -version=CI_MAIN -debug -g -unittest -w -vcolumns -of=httparsed-unittest $SRC_FILES
+    $DMD -version=CI_MAIN -debug -g -unittest -w -vcolumns -of=httparsed-unittest $SRC_FILES
     ./httparsed-unittest
 
     # unittests with possible SSE
-    $DC -version=CI_MAIN -debug -g -unittest -w -vcolumns -mcpu=native -of=httparsed-sse-unittest $SRC_FILES
+    $DMD -version=CI_MAIN -debug -g -unittest -w -vcolumns -mcpu=native -of=httparsed-sse-unittest $SRC_FILES
     ./httparsed-sse-unittest
 
     # betterC unitests no SSE
-    $DC -version=CI_MAIN -debug -g -unittest -w -vcolumns -betterC -of=httparsed-bc-unittest $SRC_FILES
+    $DMD -version=CI_MAIN -debug -g -unittest -w -vcolumns -betterC -of=httparsed-bc-unittest $SRC_FILES
     ./httparsed-bc-unittest
 
     # betterC unitests with possible SSE
-    $DC -version=CI_MAIN -debug -g -unittest -w -vcolumns -mcpu=native -betterC -of=httparsed-bc-sse-unittest $SRC_FILES
+    $DMD -version=CI_MAIN -debug -g -unittest -w -vcolumns -mcpu=native -betterC -of=httparsed-bc-sse-unittest $SRC_FILES
     ./httparsed-bc-sse-unittest
 
     # release build test
-    $DC -version=CI_MAIN -release -O -w -mcpu=native -inline -of=httparsed-release-test $SRC_FILES
+    $DMD -version=CI_MAIN -release -O -w -mcpu=native -inline -of=httparsed-release-test $SRC_FILES
     ./httparsed-release-test
 fi
